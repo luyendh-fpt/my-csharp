6837739160dccfe9f4267c469aa60fa085061999
Xuan Hung <xuanhung@Xuans-MacBook-Pro.local>
2018-05-31 10:58:55 +0700

    Tao khung StudentModel.

--- ConsoleApp1/StudentModel.cs
+++ ConsoleApp1/StudentModel.cs
@@ -1,7 +1,18 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
 {
     public class StudentModel
     {
-        
+        public void Save(Student student)
+        {
+            
+        }
+
+        public List<Student> GetList()
+        {
+            var list = new List<Student>();
+            return list;
+        }
     }
 }
